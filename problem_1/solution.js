8370b775d713bcc83bb88551564609ef4fc1bf60
Renzo Gaspary <rgaspary@gmail.com>
2017-05-01 09:46:41 -0400

    removing unnecessary files, reducing code size, adding es6.

--- problem_1/solution.js
+++ problem_1/solution.js
@@ -2,22 +2,15 @@
  * Created by renzoggaspary on 7/1/16.
  */
 "use strict";
-
-(function () {
-    let num1 = 3,
-        num2 = 5,
-        maxNum = 1000;
-    const output = document.querySelector('#answer');
-    let returnTotal = (Multiples(num1, num2, maxNum));
-    output.innerHTML = "The sum of all the multiples of " + num1 + " or " + num2 + " below " + maxNum + " is " + returnTotal;
+(() => {
+    let solution = Multiply();
+    console.log(solution);
 })();
 
-function Multiples(num1, num2, maxNum) {
+function Multiply(num1 = 3, num2 = 5, maxNum = 1000){
     let total = 0;
-    for (let i = 0; i < maxNum; i++) {
-        if (i % num1 === 0 || i % num2 === 0) {
-            total = i + total;
-        }
+    for (let i = 0; i < maxNum; i++){
+        (i % num1 === 0 || i % num2 === 0) ? total = i + total : '';
     }
     return total;
 }
